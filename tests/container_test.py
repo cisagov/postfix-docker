a518cfbe3045ae85f77df880a0c005e56819fda8
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2023-07-28 15:33:41 -0400

    Reference correct parameter name in comment
    The "stopped" parameter is no longer present now that we are using
    python-on-whales.
    
    Co-authored-by: dav3r <david.redmin@gwe.cisa.dhs.gov>

--- tests/container_test.py
+++ tests/container_test.py
@@ -20,7 +20,7 @@ VERSION_FILE = "src/version.txt"
 
 def test_container_count(dockerc):
     """Verify the test composition and container."""
-    # stopped parameter allows non-running containers in results
+    # all parameter allows non-running containers in results
     assert (
         len(dockerc.compose.ps(all=True)) == 2
     ), "Wrong number of containers were started."
